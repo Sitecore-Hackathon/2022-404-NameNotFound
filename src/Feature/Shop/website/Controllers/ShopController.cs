58dd69cbe55b86f3d6f8971a2b44e76839f52558
Cesar Yanez <Cesar.Yanez@verndale.com>
2022-03-05 14:33:53 -0500

    adding shopcontroller to create products programatically

---
+++ src/Feature/Shop/website/Controllers/ShopController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Tshirts.Feature.Shop.Models;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+
+namespace Tshirts.Feature.Shop.Controllers
+{
+    public class ShopController : ApiController
+    {
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult CreateProduct()
+        {
+            string result = "create single product invoked.";
+
+            try
+            {
+                var product = new Product();
+                //solo para probar
+                product.Name = "test prog" + DateTime.Now.Minute.ToString();
+                product.Description = "test prog desc";
+
+                CreateSingleProduct(product);
+            }
+            catch (Exception ex)
+            {
+                result = "There was an exception when creating a single product. Message: " + ex.ToString();
+            }
+
+            return Json(result);
+        }
+
+        public bool CreateSingleProduct(Product product)
+        {
+            Sitecore.Data.Database masterDb = Sitecore.Configuration.Factory.GetDatabase("master");
+
+            //Item parentItem = Sitecore.Context.Item;
+            Item productsFolder = masterDb.GetItem(new ID("{02061115-477D-4F9D-9408-91172099F828}"));
+
+            var template = masterDb.GetTemplate(new ID("{5BF87A86-D8C3-454B-870B-EFC3F32BD02E}"));
+
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                Item newItem = productsFolder.Add(product.Name, template);
+                try
+                {
+                    if (newItem != null)
+                    {
+                        newItem.Editing.BeginEdit();
+                        newItem["Name"] = product.Name;
+                        newItem["Description"] = product.Description;
+                        newItem.Editing.EndEdit();
+                    }
+
+                    return true;
+                }
+                catch
+                {
+                    newItem.Editing.CancelEdit();
+                    return false;
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ src/Feature/Shop/website/Repositories/LoadRoutes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Sitecore.Pipelines;
+
+namespace Tshirts.Feature.Shop.Repositories
+{
+    public class LoadRoutes
+    {
+        public virtual void Process(PipelineArgs args)
+        {
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RegisterRoute(RouteTable.Routes);
+        }
+
+        protected virtual void RegisterRoute(RouteCollection routes)
+        {
+            RouteTable.Routes.MapHttpRoute("CreateProduct",
+                "api/custom/createprod", /* do not include a forward slash in front of the route */
+                new { controller = "Shop", action = "CreateProduct" } /* controller name should not have the "Controller" suffix */
+            );
+        }
+    }
+}
